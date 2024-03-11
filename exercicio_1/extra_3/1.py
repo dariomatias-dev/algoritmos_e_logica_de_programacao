f6ed5ec0ffa116b5665fc90c178747f5177af9c5
dariomatias-dev <matiasdario75@gmail.com>
2024-03-11 19:53:40 -0300

    feat(exercise_1/extra_3): corrected solution 1.1.txt and added automatic resolution for question 1
    - Corrected solution 1.1.txt, which had explicit errors.
    - Automatic resolution for question 1 of the activity added to file 1.py.

--- exercicio_1/extra_3/1.py
+++ exercicio_1/extra_3/1.py
@@ -1,6 +1,57 @@
 def calculate(x, y, z):
-    w = x * y < z / x or x / y < z * x and z * y < x
-    print(w)
+    print(
+    '''
+{} * {} < {} / {} or {} / {} < {} * {} and {} * {} < {}
+    '''.format(
+            x, y, z, x, x, y, z, x, z, y, x,
+        )
+    )
+
+    divisionResult1 = z / x
+    divisionResult2 = x / y
+
+    print(
+        '''
+{} < {} or {} < {} and {} < {}
+        '''.format(
+            x * y,
+            int(divisionResult1) if int(divisionResult1) == divisionResult1 else divisionResult1,
+            int(divisionResult2) if int(divisionResult2) == divisionResult2 else divisionResult2,
+            z * x,
+            z * y,
+            x,
+        )
+    )
+
+    result1 = x * y < z / x
+    result2 = x / y < z * x
+    result3 = z * y < x
+
+    print(
+        '''
+{} or {} and {}
+        '''.format(
+            result1, result2, result3,
+        )
+    )
+
+    print(
+        '''
+{} or {}
+        '''.format(
+            result1, result2 and result3,
+        )
+    )
+
+    print(
+        '''
+{}
+        '''.format(
+            result1 or result2 and result3,
+        )
+    )
+
+    print("------------------------------------------------------------")
 
 calculate(5, 10, 15)
 calculate(40, 10, 3)
